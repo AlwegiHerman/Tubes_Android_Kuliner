3ebb4e42db0ff06afed79a09941a9683d9f2fb77
AlwegiHerman <AlwegiHerman@users.noreply.github.com>
2019-01-02 09:54:44 +0700

    tugas besar

---
+++ app/src/main/java/com/example/alwegi/kuliner/user.java
@@ -0,0 +1,16 @@
+package com.example.alwegi.kuliner;
+
+public class user {
+    public String id;
+    public String userName;
+    public String email;
+    public String password;
+
+    //method untuk mengenalkan beberapa parameter
+    public user(String id, String userName, String email, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+}
